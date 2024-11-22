8142c306fcd2df21bb3b8efecbd93d82ae41bdc2
christian <christian.cuarite@tecsup.edu.pe>
2024-11-22 15:18:13 -0500

    Inicializando el proyecto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-// src/App.js
 import React from "react";
-import WelcomePage from "./components/WelcomePage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar"; // Barra de navegación siempre visible
+import WelcomePage from "./components/WelcomePage"; // Página de bienvenida
+import RegisterForm from "./components/RegisterForm"; // Formulario de registro
+import Preferencias from "./components/Preferencias"; // Preferencias del usuario
+import Tutoriales from "./components/Tutoriales"; // Página de tutoriales
+import PrivateRoute from "./components/PrivateRoute"; // Componente para proteger rutas privadas
 
 function App() {
   return (
-    <div className="App">
-      <WelcomePage />
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/tutoriales" element={<Tutoriales />} />
+        <Route
+          path="/preferencias"
+          element={
+            <PrivateRoute>
+              <Preferencias />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
